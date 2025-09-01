802d63aed461d0dc7cf738c4b6a83c9fda78497b
XBurnsX <thexxburnsxx@gmail.com>
2025-08-31 20:04:13 -0400

    Mise à jour de la version de l'application à 1.4.3 et augmentation du code de version à 36 dans le fichier build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.xburnsx.toutiebudget"
         minSdk = 26
         targetSdk = 36
-            versionCode = 35
-    versionName = "1.4.2"
+            versionCode = 36
+    versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
